7074376bf5cce9a3dfa14f914f93af4a3ad463e4
Tish Chungoora <tishal82@gmail.com>
2019-11-18 10:38:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(nameArr, eventName) {
+    let message = [];
+
+    for (let i = 0; i < nameArr.length; i++) {
+        message.push(`Thank you, ${nameArr[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return message;
+}
+
+function countDown(startNum) {
+    while (startNum > 0) {
+        console.log(startNum);
+        startNum -= 1;
+    }
+    console.log(startNum);
+}
